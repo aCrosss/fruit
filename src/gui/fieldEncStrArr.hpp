fc084237b08ba14f4cab4fdc9dde432d1454d510
Alexey Krestianinov <stavac0@yandex.ru>
2026-07-09 12:24:00 +0500

    implement ecnoded string array field

---
+++ src/gui/fieldEncStrArr.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "encoding.hpp"
+#include "fieldBase.hpp"
+
+class FieldEncStrArr : public FieldBase {
+  private:
+    struct EncStr {
+        Gtk::Box          container;
+        Gtk::Box          subbox;
+        Gtk::Entry        entry;
+        Gtk::ComboBoxText encoding;
+        Gtk::Label        label_error;
+        Gtk::Button       btn_del;
+        sigc::connection  btn_del_con;
+    };
+    std::vector<EncStr> array;
+
+    Gtk::Box array_container;
+
+    Gtk::Box    btn_container;
+    Gtk::Button btn_add;
+
+    void validateSingleEncStr(size_t ind);
+    void reconnectSignals();
+    void addEncStr();
+    void addEncStr(nlohmann::json &j);
+    void removeEncStr(size_t ind);
+
+  public:
+    void clear() override;
+
+    void show(Gtk::Box *parent) override;
+    void hide(Gtk::Box *parent) override;
+
+    void get(nlohmann::json &j) override;
+    void set(nlohmann::json &j) override;
+
+    bool validate() override;
+
+    FieldEncStrArr(std::string tag);
+    ~FieldEncStrArr();
+};
