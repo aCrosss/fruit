0d8d98ead227477d3197a37791093941ffd3de93
Alexey Krestianinov <stavac0@yandex.ru>
2026-08-27 15:00:16 +0500

    remove unused enum FieldType

--- src/gui/guiAreaBase.hpp
+++ src/gui/guiAreaBase.hpp
@@ -7,15 +7,6 @@
 
 typedef std::shared_ptr<FieldBase> Field;
 
-enum FieldType {
-    FIELD_TYPE_BOOL,
-    FIELD_TYPE_ENC_STR,
-    FIELD_TYPE_INT,   //
-    FIELD_TYPE_CHAR,  // single numerical field
-    FIELD_TYPE_FLOAT, //
-    // FIELD_TYPE_ENUM_ENC,
-};
-
 class GUIAreaBase {
   protected:
     std::string tag;
